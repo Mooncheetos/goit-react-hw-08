7159f7231141ba12115b67b5c0d4500f52653ac1
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-05-04 11:58:42 +0300

    sevRe

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,7 +1,5 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { selectNameFilter } from "../filters/selectors";
-import { selectContacts } from "./selectors";
-
 
 export const selectContacts = (state) => state.contacts.items;
 export const selectError = (state) => state.contacts.error;
